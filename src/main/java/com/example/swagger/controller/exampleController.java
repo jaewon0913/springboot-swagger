36e3784f75ba61e9af92db7eef2d85fe5a253561
jaewon0913 <jaewon9937@gmail.com>
2020-11-15 17:12:11 +0900

    기타 수정 및 인덱스 페이지 추가

--- src/main/java/com/example/swagger/controller/exampleController.java
+++ src/main/java/com/example/swagger/controller/exampleController.java
@@ -3,23 +3,13 @@ package com.example.swagger.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/example")
 public class exampleController {
-	
-	@GetMapping("/test")
-	public String test() {
-		
-		return "swagger test project";
+
+	@RequestMapping("/")
+	public String viewPage() {
+		return "index";
 	}
-	
-	@RequestMapping("/test2")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
 }
 
